6ec4997d584ba4e146e0e601db3fa49de0ade4a7
Barbara Huber <huber.barbara@gmail.com>
2012-11-27 11:19:42 +0100

    new Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RaidMe::Application.config.secret_token = '0af5611b08fc3cc2ebf2d3eee448537a99c7c193a519656ed60fbb0aaac83b9e53108d7021f1c6b719373e4cca9a9102534f9c331393d78d8d6462c9eba2db0d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RaidMe::Application.config.session_store :cookie_store, key: '_RaidMe_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RaidMe::Application.config.session_store :active_record_store
